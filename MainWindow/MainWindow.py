a939a2ffbc0750ec8763d00f5f00a9cd3e941d00
Anthony Lyappiev <archlinux@antavr.ru>
2014-09-21 03:07:31 +0400

    -

--- MainWindow/MainWindow.py
+++ MainWindow/MainWindow.py
@@ -6,15 +6,20 @@ from PyQt5.uic import loadUi
 
 fileDirName = os.path.dirname(__file__)
 
+
 class MainWindow(QMainWindow):
     """
     Главное окно
     """
     def __init__(self, *args):
+        """
+
+        :param args:
+        """
         super(MainWindow, self).__init__(*args)
-        print()
+        print(__name__)
         loadUi(os.path.join(fileDirName, 'UI_' + __class__.__name__ + '.ui'), self)
 
     @pyqtSlot()
     def on_actionAboutQt_triggered(self):
-         QApplication.instance().aboutQt()
+        QApplication.instance().aboutQt()
