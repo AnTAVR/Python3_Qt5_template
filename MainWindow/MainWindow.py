9189571681afa1dde9912e04848be044793494f3
Anthony Lyappiev <archlinux@antavr.ru>
2014-09-21 02:27:02 +0400

    start

---
+++ MainWindow/MainWindow.py
@@ -0,0 +1,20 @@
+import os
+
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.uic import loadUi
+
+fileDirName = os.path.dirname(__file__)
+
+class MainWindow(QMainWindow):
+    """
+    Главное окно
+    """
+    def __init__(self, *args):
+        super(MainWindow, self).__init__(*args)
+        print()
+        loadUi(os.path.join(fileDirName, 'UI_' + __class__.__name__ + '.ui'), self)
+
+    @pyqtSlot()
+    def on_actionAboutQt_triggered(self):
+         QApplication.instance().aboutQt()
---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import sys
+
+from PyQt5.QtWidgets import QApplication
+from MainWindow.MainWindow import MainWindow
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    mainWindow = MainWindow()
+    mainWindow.show()
+
+    sys.exit(app.exec_())
